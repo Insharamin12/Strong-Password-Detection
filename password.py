34c2e6371a222c33814930cb8c39bc4f56f2d6bf
Insha Ramin <130788673+Insharamin12@users.noreply.github.com>
2024-02-15 23:32:46 +0530

    Update password.py

--- password.py
+++ password.py
@@ -14,7 +14,7 @@ def is_strong_password(password):
 passwords = [
     "Abcdefg1",   # Strong password
     "password",   # Weak password (no uppercase, no digit, short)
-    "PASSWORD",   # Weak password (no lowercase, no digit, short)
+    "PAS12@SwORD", # Strong password 
     "12345678",   # Weak password (no uppercase, no lowercase)
     "AbCdEfGh",   # Weak password (no digit, short)
     "abcdefgh"    # Weak password (no uppercase, no digit, short)
